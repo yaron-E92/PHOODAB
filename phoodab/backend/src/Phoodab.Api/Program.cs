12a757cd44e1d78d612f5669a93d5dc2a229dad1
Yaron Efrat <yaronefrat92@gmail.com>
2026-05-19 15:13:49 +0200

    Remove synthetic replenishment API data

--- phoodab/backend/src/Phoodab.Api/Program.cs
+++ phoodab/backend/src/Phoodab.Api/Program.cs
@@ -25,27 +25,11 @@ app.MapGet("/version", () =>
 .WithName("GetVersion")
 .WithOpenApi();
 
-
 var suggestionService = new ReplenishmentSuggestionService();
 
 app.MapGet("/replenishment/suggestions", () =>
 {
-    var milk = new ItemDefinition(Guid.Parse("11111111-1111-1111-1111-111111111111"), "Milk", ItemKind.Consumable);
-    var beans = new ItemDefinition(Guid.Parse("22222222-2222-2222-2222-222222222222"), "Beans", ItemKind.Consumable);
-
-    var milkEntry = new InventoryEntry(Guid.NewGuid(), milk);
-    milkEntry.AddLot(new InventoryLot(Guid.NewGuid(), milk.Id, Quantity.From(1), new Unit("liter"), null));
-
-    var beansEntry = new InventoryEntry(Guid.NewGuid(), beans);
-    beansEntry.AddLot(new InventoryLot(Guid.NewGuid(), beans.Id, Quantity.From(3), new Unit("can"), null));
-
-    var rules = new[]
-    {
-        new ReplenishmentRule(Guid.NewGuid(), milk.Id, Quantity.From(2), new Unit("liter")),
-        new ReplenishmentRule(Guid.NewGuid(), beans.Id, Quantity.From(2), new Unit("can"))
-    };
-
-    var suggestions = suggestionService.GetSuggestions(rules, new[] { milkEntry, beansEntry });
+    var suggestions = suggestionService.GetSuggestions(Array.Empty<ReplenishmentRule>(), Array.Empty<InventoryEntry>());
     return Results.Ok(suggestions);
 })
 .WithName("GetReplenishmentSuggestions")
--- phoodab/backend/tests/Phoodab.Api.Tests/SmokeTests.cs
+++ phoodab/backend/tests/Phoodab.Api.Tests/SmokeTests.cs
@@ -39,14 +39,13 @@ public class SmokeTests
     }
 
     [Test]
-    public async Task Replenishment_Suggestions_Endpoint_Returns_Ok_With_Required_Amount_And_Unit()
+    public async Task Replenishment_Suggestions_Endpoint_Returns_Ok_With_Array_Response()
     {
         var response = await _client.GetAsync("/replenishment/suggestions");
 
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
         var content = await response.Content.ReadAsStringAsync();
-        Assert.That(content, Does.Contain("requiredAmount"));
-        Assert.That(content, Does.Contain("unit"));
+        Assert.That(content.TrimStart(), Does.StartWith("["));
     }
 }
